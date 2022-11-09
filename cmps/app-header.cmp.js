4621dc31074d1164ea09d86a644e7900dec5b9e4
Eyal Yehiel <eyalyhi@gmail.com>
2022-11-09 11:41:32 +0200

    added style

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -1,10 +1,11 @@
 export default {
-	template: `
+    template: `
         <header class="app-header">
-            <h1>AppSus</h1>
+            <h1>Appsus</h1>
             <nav>
                 <router-link to="/">Home</router-link> | 
-                <router-link to="/about">About</router-link>
+                <router-link to="/about">About</router-link> | 
+                <router-link to="/email" >Email</router-link>
             </nav>
         </header>
     `,
