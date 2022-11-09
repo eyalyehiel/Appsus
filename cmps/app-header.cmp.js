69a9cb0d93cf8b4415e8ef3da993f44f62682f30
Eyal Yehiel <eyalyhi@gmail.com>
2022-11-09 23:37:52 +0200

    fixed bugs

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -1,7 +1,7 @@
 export default {
     template: `
         <header class="app-header">
-            <h1><img :src="imgUrl"/>Appsus</h1>
+            <h1><img class="img-logo" :src="imgUrl"/>Appsus</h1>
             <nav>
                 <router-link @click="changeLogo('home')" to="/">Home</router-link> | 
                 <router-link @click="changeLogo('about')" to="/about">About</router-link> | 
@@ -18,8 +18,8 @@ export default {
     methods: {
         changeLogo(value){
             switch(value){
-                case 'home': break;
-                case 'about': break;
+                case 'home': this.imgUrl="assets/img/home.png"; break;
+                case 'about': this.imgUrl="assets/img/mail.png";break;
                 case 'email': this.imgUrl="assets/img/mail.png"; break;
                 case 'keep': this.imgUrl="assets/img/icons/keep48dp.png";break;
             }
