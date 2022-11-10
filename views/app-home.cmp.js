c355ab5ab39b8b34272ae0e10c886ef1043dedfc
ShonChassel <gershoni1234@gmail.com>
2022-11-10 15:40:49 +0200

    push

--- views/app-home.cmp.js
+++ views/app-home.cmp.js
@@ -1,15 +1,15 @@
 export default {
 	template: `
-        <section class="home-page">
-        
-        </section>
-<div class="container-font">
-  <h1>
-    <i></i>
-    <span id="a">APPSUS</span>
-    <span id="b">IS</span>
-    <span id="c">AWESOME</span>
-  </h1>
-</div>
+<section class="home-page">
+    <div class="container-font">
+       <h1>
+       <i></i>
+       <span id="a">APPSUS</span>
+       <span id="b">IS</span>
+       <span id="c">AWESOME</span>
+      </h1>
+    </div>
+</section>
+
     `,
 }
